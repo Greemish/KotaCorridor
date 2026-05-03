2a43caa9ac3743339ecdc43b42a0d7ac52189290
main <phomolo9686@gmail.com>
2026-05-03 18:26:55 +0200

    Initial commit

---
+++ src/main/java/com/example/KotaCorridor/KotaCorridorApplication.java
@@ -0,0 +1,13 @@
+package com.example.KotaCorridor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KotaCorridorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KotaCorridorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/KotaCorridor/KotaCorridorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.KotaCorridor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KotaCorridorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
